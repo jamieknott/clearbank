5e5ae17e0d08a005960c4a5a78967624cea72a8d
jamieknott <jamiejk2001@gmail.com>
2025-09-16 21:30:17 +0100

    Remove dupe data store and decouple service from data access

--- ClearBank.DeveloperTest/Services/DataService.cs
+++ ClearBank.DeveloperTest/Services/DataService.cs
@@ -1,18 +1,30 @@
 ﻿using ClearBank.DeveloperTest.Interfaces;
 using ClearBank.DeveloperTest.Types;
+using System.Configuration;
 
 namespace ClearBank.DeveloperTest.Services
 {
     public class DataService : IDataService
     {
-        public Account GetAccount(string accountNumber)
+        private readonly IAccountDataStore _primaryAccountDataStore;
+        private readonly IAccountDataStore _backupAccountDataStore;
+
+        private readonly string _dataStoreType;
+
+        public DataService(IAccountDataStore primaryAccountDataStore, IAccountDataStore backupAccountDataStore)
         {
-            throw new System.NotImplementedException();
+            _primaryAccountDataStore = primaryAccountDataStore; // configure primary data store connection
+            _backupAccountDataStore = backupAccountDataStore; // configure backup data store connection
+
+            _dataStoreType = ConfigurationManager.AppSettings["DataStoreType"];
         }
 
-        public void UpdateAccount(Account account)
-        {
-            throw new System.NotImplementedException();
-        }
+        // Followed initial setup here but would rather rely on a backup as a fallback instead of toggling through config.
+        private IAccountDataStore DataStore =>
+            _dataStoreType == "Backup" ? _backupAccountDataStore : _primaryAccountDataStore;
+
+        public Account GetAccount(string accountNumber) => DataStore.GetAccount(accountNumber);
+
+        public void UpdateAccount(Account account) => DataStore.UpdateAccount(account);
     }
 }
--- README.md
+++ README.md
@@ -5,13 +5,19 @@ Create tests first to ensure logic remains unchanged after refactor.
 Concrete DataStore from PaymentService could be removed adding an interface layer.
 * Dupe stores, could be one?
 
+There are methods for GetAccount and UpdateAccount but nothing for actioning the payment? (send to creditor)
+* Going to assume this is out of scope for this tech test...
+	* If it wasn't out of scope, the call would only happen if the scheme is valid before we update the account info.
+
 PaymentScheme switch refactor to remove the long conditional.
 * Factory?
 	* Easy to extend
 
 ### IMPLEMENTATION
 Create DataService to add a layer of abstraction between the PaymentService and AccountDataStore
-* DataService will ??
+* DataService will control DataStore for both primary and backup, removing need for potential dupe code.
 
 Create Scheme factory to replace the switch section making the PaymentService cleaner and isolates the validation logic.
 * Easier to add new schemas and update existing ones.
+
+### FINAL NOTES
